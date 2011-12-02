f0b15c31037ff54ed1bdad9e33b18a47f4781d4e
Julien Desrosiers <juliend2@gmail.com>
2011-12-02 18:07:37 -0500

    use a class to store williams methods (ssh & local for now)

--- lib/william.rb
+++ lib/william.rb
@@ -1,17 +1,45 @@
 require "william/version"
 
+def create_class(class_name, superclass, &block)
+  klass = Class.new superclass, &block
+  Object.const_set class_name, klass
+end
+
 module William
+
+  class Commands
+    def initialize(host)
+      @host = host
+    end
+
+    def local(command)
+      puts `#{command}`
+    end
+
+    def ssh(command)
+      puts `ssh #{@host} '#{command}'`
+    end
+  end
+
   class Runner
     def initialize(file_path)
       @file_path = file_path
     end
 
     def run(args)
-      load @file_path
+      file_content = File.open(@file_path).read
 
-      args.each do |arg|
-        send arg.to_sym
+      create_class "MyCommands", Commands do
+        self.class_eval file_content
+      end
+
+      $remotes.each do |remote|
+        args.each do |arg|
+          commander = MyCommands.new(remote)
+          commander.send arg.to_sym
+        end
       end
     end
+
   end
 end
